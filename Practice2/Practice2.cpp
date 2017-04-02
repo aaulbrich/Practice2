2d645b6030bf0840cb0e03722f7343eb1e3182bd
Andrew Ulbrich <andrew@ulbrichtech.com>
2017-04-01 20:00:31 -0500

    Commit

--- Practice2/Practice2.cpp
+++ Practice2/Practice2.cpp
@@ -1,11 +1,45 @@
-// Practice2.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <limits>
 
+using namespace std;
 
 int main()
 {
-    return 0;
+	for (; ; ) {
+		double base(0);
+		double power(0);
+		double a(1);
+		cout << "Enter a base number:" << endl;
+		cin >> base;
+		if (cin.fail()) {
+			cout << "Enter a valid number" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '/n');
+		}
+		else if (cin.good()) {
+			cout << "Enter the exponent number:" << endl;
+			cin >> power;
+			if (power > 0) {
+				for (int t = 0; t < power; t++) {
+					a *= base;
+				}
+				cout << a << endl;
+			}
+			else if (power < 0) {
+				for (int t = 0; t > power; t--) {
+					a /= base;
+				}
+				cout << a << endl;
+				cin.sync();
+				cin.clear();
+			}
+			else if ((base || power) == 0 || cin.fail()) {
+				cout << "Enter a valid number" << endl;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '/n');
+			}
+		}
+	}
+	return 0;
 }
-
